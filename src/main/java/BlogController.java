a1578fae6aeb66f1d7d70a5d472daa04f925cc96
Ryujin-X <joseph.mcevoy1984@gmail.com>
2016-07-11 18:06:01 +0100

    make it better

---
+++ src/main/java/BlogController.java
@@ -0,0 +1,41 @@
+package blog;
+
+import java.util.ArrayList;
+//import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BlogController{
+
+	public ArrayList<Post> allPosts = new ArrayList<Post>();
+	
+	@RequestMapping(value="/createPost", method=RequestMethod.GET)
+	public String viewPosts(Model model){
+
+		//Add a key/value pair to the "model" map (aka: hash, hasmap, dict etc..)
+		model.addAttribute("post", new Post());
+
+		//return a html template named posts
+		return "addPost";
+	}
+
+	@RequestMapping(value="/createPost", method=RequestMethod.POST)
+	public String createPost(@ModelAttribute Post post, Model model){
+		//LocalDateTime currentTime = LocalDateTime.now();
+		LocalDate currentDate = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		String formattedDate = currentDate.format(formatter);
+		post.setDate(formattedDate);
+		allPosts.add(0, post);
+		model.addAttribute("posts", allPosts);
+		return "posts";
+	}
+	
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,32 @@
+package blog;
+
+public class Post{
+	
+	private String date;
+	private String title;
+	private String content;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setTitle(String title){
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setDate(String date){
+		this.date = date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content){
+		this.content = content;
+	}
+}
